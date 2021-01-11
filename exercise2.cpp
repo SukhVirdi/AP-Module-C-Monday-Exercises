8e49203a3013796faf0b37378b00ea4a352bb404
SukhVirdi <56550489+SukhVirdi@users.noreply.github.com>
2021-01-11 19:44:39 +0000

    small refactor and renamed variables

--- exercise2.cpp
+++ exercise2.cpp
@@ -1,6 +1,6 @@
 //https://repl.it/join/cwxpapeo-sukhvirdi7
-//In the program below I used two options for temperature conversion. If the user does not match then it will display an error message.
-//I utilised an if statement based on the request it will execute the corresponding calculation, which is then concatonated with all the inputs to show the output.
+//In the code below I made a basic temperature conversion program with F and C convert MVP. If the user input is not the right data type then display an error/invalid input message. The user can also enter either uppercase/lowercase options when choosing their units.
+//I utilised an if statement based on the request it will execute the corresponding calculation, which is then concatonated with all the inputs display the desired output.
 
 #include<iostream>
 using namespace std;
@@ -8,27 +8,27 @@ using namespace std;
 //main program
 int main()
 {
-  float temp, fahr, cen;
+  float temperature, fahrenheit, centigrade;
   char option;
   
   //asks user to enter the starting temperature
   cout << "Please enter the starting temperature: ";
-  cin >> temp;
+  cin >> temperature;
   cout << "Press ‘C’ to convert from Fahrenheit to Centigrade." << endl;
   cout << "Press ‘F’ to convert from Centigrade to Fahrenheit." << endl;
   cin >> option;
 
   //option for converting Fahrenheit into Centigrade
   if (option == 'C' || option == 'c') {
-  cen = ((temp - 32) / 1.8); //temperature conversion 
+  centigrade = ((temperature - 32) / 1.8); //temperature conversion 
   cout << "Your choice is " << option << endl;
-  cout << temp << " degrees Fahrenheit is " << cen  << endl;
+  cout << temperature << " degrees Fahrenheit is " << centigrade  << endl;
   }
-  //option for converting Centigrade into Fahernheit
+  //option for converting Centigrade into Fahrenheit
   else if (option == 'F' || option == 'f') {
-  fahr = ((1.8 * temp) + 32.0);
+  fahrenheit = ((1.8 * temperature) + 32.0);
   cout << "Your choice is " << option << endl;
-  cout << temp << " degrees Centigrade is " << fahr  << endl;
+  cout << temperature << " degrees Centigrade is " << fahrenheit  << endl;
   }
   //wrong input is shown if the user inputs a non-float and exits program
   else
